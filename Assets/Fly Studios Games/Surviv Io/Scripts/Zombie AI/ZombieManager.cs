63955c78edbfbdbb4fc3199f5b85611280ae42b0
Traffic Wolf <flywinlucky777@gmail.com>
2025-12-01 18:45:31 +0200

    j

--- Assets/Fly Studios Games/Surviv Io/Scripts/Zombie AI/ZombieManager.cs
+++ Assets/Fly Studios Games/Surviv Io/Scripts/Zombie AI/ZombieManager.cs
@@ -172,7 +172,7 @@ public class ZombieManager : MonoBehaviour
 
     private int ComputeSpawnCountForWave(int wave)
     {
-        return Mathf.Max(1, initialCount + (wave - 1) * spawnCountIncrementPerWave * 4);
+        return Mathf.Max(1, initialCount + (wave - 1) * spawnCountIncrementPerWave * 5);
     }
 
     private float ComputeZombieAttackDamage(int wave)
